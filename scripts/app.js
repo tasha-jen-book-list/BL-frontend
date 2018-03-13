4d8f77a63401614b15fe83607f7cb150902460e1
Jen Lipton <jkl.lipton@gmail.com>
2018-03-13 16:11:52 -0700

    debugging

--- scripts/app.js
+++ scripts/app.js
@@ -5,12 +5,11 @@
     const Book = module.Book;
     const bookView = module.bookView;
 
-    page('BL-frontend/', () => Book.fetchAll(bookView.init));
-    page('BL-frontend/books/new', () => bookView.initNew());
-    page('BL-frontend/books/:id', (ctx) => Book.fetchOne(ctx.params.id, bookView.initDetail));
-    page('/igloo', ()=> console.log('bucket'));
+    page('/home', () => Book.fetchAll(bookView.init));
+    page('/books/new', () => bookView.initNew());
+    page('/books/:id', (ctx) => Book.fetchOne(ctx.params.id, bookView.initDetail));
 
-    page('*', () => page.redirect('/BL-frontend/'));
+    page('*', () => page.redirect('/home'));
 
     page({ hashbang: true });
 
