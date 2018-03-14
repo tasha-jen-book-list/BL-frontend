10348def2442c09999f99bcc5a3c11ea7d341d4d
Jen Lipton <jkl.lipton@gmail.com>
2018-03-14 09:44:43 -0700

    debugging

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -34,11 +34,11 @@
                 event.preventDefault();
             
                 const data = {
-                    title: $('textarea[name=title]').val(),
-                    author: $('textarea[name=author]').val(),
-                    isbn: $('textarea[name=isbn]').val(),
-                    image_url: $('textarea[name=image_url]').val(),
-                    description: $('textarea[name=description]').val()
+                    title: $('input[name=title]').val(),
+                    author: $('input[name=author]').val(),
+                    isbn: $('input[name=isbn]').val(),
+                    image_url: $('input[name=image_url]').val(),
+                    description: $('input[name=description]').val()
                 };
 
                 Book.create(data, (book) => {
